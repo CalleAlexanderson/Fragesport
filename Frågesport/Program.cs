636bea686f03c0135aa852f9cbdd7578aceb0eec
Calle <calle.alexanderson@elev.ga.ntig.se>
2020-09-24 14:48:21 +0200

    andra commit

--- Frågesport/Program.cs
+++ Frågesport/Program.cs
@@ -6,7 +6,66 @@ namespace Frågesport
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int question = 0;
+            int points = 0;
+            int game = 0;
+            Console.WriteLine("Det hät är en frågesport om League of legends items  \n skriv s för att starta");
+            string start = Console.ReadLine().ToLower();
+
+            while (start == "s")
+            {
+                game = 1;
+            }
+
+            while (game == 1)
+            {
+                question++;
+                game = 0;
+            }
+
+            if (question == 1 && game == 1)
+            {
+
+                string answer1 = AnswerQuestion();
+
+                Question1();
+                if (answer1 == "c")
+                {
+                    Console.WriteLine("korrekt");
+                    points++;
+                    game = 1;
+                }
+                else
+                {
+                    Console.WriteLine("Inkorrekt");
+                    game = 1;
+                }
+
+            }
+
+
+
+
+        }
+
+        static void Question1()
+        {
+            Console.WriteLine("Hur många stacks har black cleavers armor shred passsive? \n A: 4 \n B: 5 \n C: 6 \n D: 7");
+        }
+
+        static string AnswerQuestion()
+        {
+            string answer = "";
+
+            while (answer != "a" && answer != "b" && answer != "c" && answer != "d")
+            {
+                Console.WriteLine("Only a, b, c, or d is a valid answer");
+                answer = Console.ReadLine().ToLower();
+            }
+            return answer;
+
         }
     }
+
+
 }
